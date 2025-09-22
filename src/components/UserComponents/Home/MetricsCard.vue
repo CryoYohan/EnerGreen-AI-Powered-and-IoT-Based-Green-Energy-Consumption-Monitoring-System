3ca7d230837e0838dd0b728f77796cfc9cf10436
CryoYohan <cyrilypil@gmail.com>
2025-09-22 20:57:03 +0800

    [feat]: Accurate summation of kwh consumption of today and new charts

--- src/components/UserComponents/Home/MetricsCard.vue
+++ src/components/UserComponents/Home/MetricsCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div 
     id="metrics" 
-    class="flex w-[100%] self-center flex-wrap justify-around gap-4 p-5 w font-poppins md:gap-8"
+    class="flex w-[100%] self-center flex-wrap justify-around gap-4 p-5 font-poppins md:gap-8"
   >
-      <div 
+    <div 
       v-for="(metric, index) in metrics" 
       :key="index"
       class="flex flex-col w-full sm:w-[300px] p-6 bg-white shadow rounded-lg"
@@ -22,40 +22,11 @@
 
 <script>
 export default {
-  data() {
-    return {
-      metrics: [
-        {
-          title: 'Current Cost',
-          icon: '/src/Images/Icons/Peso.svg',
-          cost: '₱12.30',
-          definition: 'Current rate'
-        },
-        {
-          title: 'Consumption',
-          icon: '/src/Images/Icons/electric.svg',
-          cost: '0.85 kWh',
-          definition: 'Today'
-        },
-        {
-          title: 'Peak Usage',
-          icon: '/src/Images/Icons/Usage.svg',
-          cost: '6:00 PM - 9:00 PM',
-          definition: 'Today'
-        },
-        {
-          title: 'Solar Generation',
-          icon: '/src/Images/Icons/sun.svg',
-          cost: '3.2 kWh',
-          definition: 'Today'
-        },
-        {
-          title: 'CO₂ Saved',
-          icon: '/src/Images/Icons/leaf.svg',
-          cost: '1.5 kg',
-          definition: 'Today'
-        },
-      ]
+  name: "MetricsCard",
+  props: {
+    metrics: {
+      type: Array,
+      required: true
     }
   }
 }
